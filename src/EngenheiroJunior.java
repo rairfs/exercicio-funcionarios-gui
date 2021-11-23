23b2dc3c683164cc265ec3d96bbab3227defa2ee
Rai Rafael <rairafaelss@gmail.com>
2021-11-23 00:40:33 -0300

    Criada a classe Engenheiro Junior

---
+++ src/EngenheiroJunior.java
@@ -0,0 +1,20 @@
+
+import java.util.Random;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Rai_R
+ */
+public class EngenheiroJunior extends Funcionario {
+    public EngenheiroJunior(){
+        Random r = new Random();
+        salario = 1000 + 1500 * r.nextFloat();
+        categoria = 1;
+    }
+    
+}
