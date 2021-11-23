1c7335fbd36b1b3e45cd01ccbeb351a3f16596be
Rai Rafael <rairafaelss@gmail.com>
2021-11-23 00:38:15 -0300

    Criada a classe Assistente Administrativo

--- src/AssistenteAdministrativo.java
+++ src/AssistenteAdministrativo.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,6 +12,8 @@
  */
 public class AssistenteAdministrativo extends Funcionario {
     public AssistenteAdministrativo (){
-        
+        Random r = new Random ();
+        salario = 500 + 1000 * r.nextFloat();
+        categoria = 0;
     }
 }
